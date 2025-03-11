76eabafbb323c99a42d34751b066ce309da95bbb
rede.wijaya <rede.wijaya@liquibit.io>
2025-03-11 22:11:36 +0700

    add todo detail test

--- tests/test_todos.py
+++ tests/test_todos.py
@@ -1,4 +1,4 @@
-from tests.data import MOCK_TODOS_RESPONSE
+from tests.data import MOCK_TODO_RESPONSE, MOCK_TODOS_RESPONSE
 
 
 def test_list_todos(client, mocker):
@@ -12,3 +12,10 @@ def test_list_todo_endpoint(client, mocker):
     response = client.get("/todos")
     assert response.status_code == 200
     assert response.json == MOCK_TODOS_RESPONSE
+
+
+def test_detail_todo_endpoint(client, mocker):
+    mocker.patch("router.todo.get_todo_by_id", return_value=MOCK_TODO_RESPONSE)
+    response = client.get("/todos/1")
+    assert response.status_code == 200
+    assert response.json == MOCK_TODO_RESPONSE
